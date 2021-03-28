60c80e44539920614eed9a6ae7d075e945214ec5
carez <carezd@gmail.com>
2021-03-28 19:48:36 +0200

    Refactor main.py old code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from classes import Board
 
+
 def initialize_game(pl1, pl2, winning_points):
     pl1_human = pl1['type'] == 'human'
     pl2_human = pl2['type'] == 'human'
@@ -18,14 +19,13 @@ def initialize_game(pl1, pl2, winning_points):
     )
     return board
 
-def evaluate_actions(actions,board): ###DUMMY EVAL FUNC
-    return random.choice(random.choice(actions))
 
 def run_game(pl1, pl2, winning_points, interactive=True):
     pl1_depth = pl1.get('parameters', {}).get('depth', 6)
     pl2_depth = pl2.get('parameters', {}).get('depth', 6)
 
     iteration_count = 0
+    max_iterations = winning_points * 30
     value=0
     pl1_time = 0
     pl2_time = 0
@@ -36,24 +36,26 @@ def run_game(pl1, pl2, winning_points, interactive=True):
         gridlock_winner, score_winner = board.terminal_test()
         if any((gridlock_winner, score_winner)):
             break
-        if iteration_count > winning_points*30:
-            print(f'pl1 actions={board.pl[0].get_actions(board)}')
-            print(f'pl2 actions={board.pl[1].get_actions(board)}')
-            raise RecursionError(f'MAX_ITERATIONS ({winning_points*30}) reached')
+        if iteration_count > max_iterations:
+            print(f'  pl1 actions={board.pl[0].get_actions(board)}')
+            print(f'  pl2 actions={board.pl[1].get_actions(board)}')
+            print("  Value of last move: ", value)
+            print(f'  gridlock_winner={gridlock_winner}, score_winner={score_winner}')
+            board.display_board()
+            raise RecursionError(f'MAX_ITERATIONS ({max_iterations}) reached')
         if interactive:
             board.display_board()
             print("Value of last move: ",value)
-#        input("Press enter to progress")
+        # input("Press enter to progress")
 
         cur_pl = board.pl[board.pl_turn]
         actions = cur_pl.get_actions(board)
         if len(actions) != 0:
-            current_player_cannot_move = False
-            if cur_pl.human == False:
+            if not cur_pl.human:
                 if cur_pl.pl_id == -1: 
                     start = timer()
                     if pl1['type'] == 'random':
-                        value, chosen_action = None, evaluate_actions(actions, board)
+                        value, chosen_action = None, random.choice(random.choice(actions))
                     else:
                         value, chosen_action = board.min_alpha_beta(-1000,1000,pl1_depth, me=0)
                     end = timer()
@@ -61,7 +63,7 @@ def run_game(pl1, pl2, winning_points, interactive=True):
                 else:
                     start = timer()
                     if pl2['type'] == 'random':
-                        value, chosen_action = None, evaluate_actions(actions, board)
+                        value, chosen_action = None, random.choice(random.choice(actions))
                     else:
                         value, chosen_action = board.max_alpha_beta(-1000,1000,pl2_depth, me=1)
                     end = timer()
@@ -81,7 +83,7 @@ def run_game(pl1, pl2, winning_points, interactive=True):
             chosen_action = None
             value = "NA"
         board.update_state(chosen_action)
-        
+
         if interactive:
             print("Score: {}".format(board.pl_scores))
     if score_winner:
@@ -99,31 +101,24 @@ def run_game(pl1, pl2, winning_points, interactive=True):
 
 
 if __name__ == "__main__":
-    pl2 = {
-        'name': 'human',
-        'type': 'human',
-    }
-    # pl1 = {
-    #     'name': 'hminimax-depth3-e1',
-    #     'type': 'hminimax',
-    #     'parameters': {
-    #         'depth': 3,
-    #         'eval_type': 'default',
-    #     }
+    # pl2 = {
+    #     'name': 'human',
+    #     'type': 'human',
     # }
     pl1 = {
-        'name': 'hminimax-depth3', # any string
-        'type': 'hminimax', # human | hminimax | random
-        'parameters': {     # dependant on player_type
-            'depth': 3,
+        'name': 'hminimax-depth5', # any string
+        'type': 'hminimax',        # human | hminimax | random
+        'parameters': {            # algorithm parameters
+            'depth': 5,
+            'row_score': [0.2],
         }
     }
     pl2 = {
-        'name': 'hminimax-depth4',
+        'name': 'hminimax-depth5',
         'type': 'hminimax',
         'parameters': {
-            'depth': 4,
-            'eval_type': 'default',
+            'depth': 5,
+            'row_score': [0.2],
         }
     }
     winning_points = 10
