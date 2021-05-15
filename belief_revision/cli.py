80f275a4aa92b8b6c6e8d3a7b16cdc4c7ce23f5b
carez <carezd@gmail.com>
2021-05-15 23:26:45 +0200

    Fix(cli): Crash when some commands receive no arguments

--- belief_revision/cli.py
+++ belief_revision/cli.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 import cmd
 import inspect
 
@@ -15,57 +14,6 @@ logging.basicConfig(
 )
 
 
-def main1():
-    KB = Knowledge_base()
-
-    #Use sympy to return boolean value
-    x,y,r,s = symbols("x y r s")
-    ###Resolve test
-    alpha = KB.fetch_sample_thesis()
-    premises = []
-    for premis in KB.premises:
-        premises += [premis[0]]
-    print("Resolution: ",PL_resolution(premises,alpha))
-    
-    exp = Implies(x,y)
-    print(exp.subs(x, False))
-
-    #Example add a premise to KB
-    [print(f"rank {prem[1]} {prem[0]}") for prem in KB.premises]
-    KB.add_premise(exp,5)
-    print(KB)
-    KB.add_premise(exp,"2")
-    print(KB)
-    KB.add_premise(exp,4)
-    print(KB)
-
-    print("#"*20)
-    [print(f"rank {prem[1]} {prem[0]}") for prem in KB.premises]
-    print("#" * 20)
-
-    #Try to make to_clause function to work with example from book:
-    """Example from book: Chapter 7.5.2 Proof by resolution (book version 4), subsection "Conjunctive normal form"
-    B1, 1 ⇔ (P1, 2 ∨ P2, 1) should be turned into 3 clauses:
-    (¬B1, 1 ∨ P1, 2 ∨ P2, 1) ∧ (¬P1, 2 ∨ B1, 1) ∧ (¬P2, 1 ∨ B1, 1)
-    """
-    b1, p1, p2 = symbols("b1 p1 p2")
-    s = symbols("s")
-    exp1 = Biconditional(b1, Or(p1, p2))
-    exp2 = Or(s, s)
-    ranks = np.arange(2)
-    premises = [to_cnf(exp) for exp in [exp1, exp2]]
-    init_exp = list(zip(premises, ranks))
-    clauses = KB.to_clauses(init_exp)
-    print(clauses)
-
-def main2():
-    print('CONTRACTION')
-    p, q, r = symbols('p q r')
-    KB = Knowledge_base()
-    print(KB)
-    KB.contract(p)
-    print(KB)
-
 class Cli(cmd.Cmd):
     # intro = '\nBelief Revision engine\n\n'
     prompt = '> '
@@ -74,15 +22,24 @@ class Cli(cmd.Cmd):
         self.kb = kb
     def do_expand(self, line):
         'Add a belief to the KB. Example: "expand p>>q"'
+        if not line:
+            self.do_help('expand')
+            return
         belief = sympify(line)
         rank = input('Enter the rank for this belief (e.g. "0.6"): ')
         self.kb.add_premise(belief, float(rank))
     def do_contract(self, line):
         'Remove a belief from the KB. Example: "contract p>>q"'
+        if not line:
+            self.do_help('contract')
+            return
         belief = sympify(line)
         self.kb.contract(belief)
     def do_revise(self, line):
         'Add a belief and delete other beliefs as necessary to keep consistency. Example: "revise p>>q"'
+        if not line:
+            self.do_help('revise')
+            return
         belief = sympify(line)
         rank = input('Enter the rank for this belief (e.g. "0.6"): ')
         self.kb.revise(belief, float(rank))
@@ -119,6 +76,52 @@ class Cli(cmd.Cmd):
     def postloop(self):
         print('\n\nFinal KB:')
         self.do_print('')
+
+
+
+# def main1():
+#     KB = Knowledge_base()
+
+#     #Use sympy to return boolean value
+#     x,y,r,s = symbols("x y r s")
+#     ###Resolve test
+#     alpha = KB.fetch_sample_thesis()
+#     premises = []
+#     for premis in KB.premises:
+#         premises += [premis[0]]
+#     print("Resolution: ",PL_resolution(premises,alpha))
+    
+#     exp = Implies(x,y)
+#     print(exp.subs(x, False))
+
+#     #Example add a premise to KB
+#     [print(f"rank {prem[1]} {prem[0]}") for prem in KB.premises]
+#     KB.add_premise(exp,5)
+#     print(KB)
+#     KB.add_premise(exp,"2")
+#     print(KB)
+#     KB.add_premise(exp,4)
+#     print(KB)
+
+#     print("#"*20)
+#     [print(f"rank {prem[1]} {prem[0]}") for prem in KB.premises]
+#     print("#" * 20)
+
+#     #Try to make to_clause function to work with example from book:
+#     """Example from book: Chapter 7.5.2 Proof by resolution (book version 4), subsection "Conjunctive normal form"
+#     B1, 1 ⇔ (P1, 2 ∨ P2, 1) should be turned into 3 clauses:
+#     (¬B1, 1 ∨ P1, 2 ∨ P2, 1) ∧ (¬P1, 2 ∨ B1, 1) ∧ (¬P2, 1 ∨ B1, 1)
+#     """
+#     b1, p1, p2 = symbols("b1 p1 p2")
+#     s = symbols("s")
+#     exp1 = Biconditional(b1, Or(p1, p2))
+#     exp2 = Or(s, s)
+#     ranks = np.arange(2)
+#     premises = [to_cnf(exp) for exp in [exp1, exp2]]
+#     init_exp = list(zip(premises, ranks))
+#     clauses = KB.to_clauses(init_exp)
+#     print(clauses)
+
 
 if __name__ == "__main__":
     KB = Knowledge_base()
